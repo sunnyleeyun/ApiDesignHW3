cb0238e86edde93dbe4c98319985753a955058bc
Zihao Yang <zihaoyang@Zihaos-MacBook-Pro-2.local>
2023-09-22 19:44:14 -0400

    create Java project using mvn

---
+++ hw3/src/main/java/edu/cmu/cs780/hw3/Game.java
@@ -0,0 +1,13 @@
+package edu.cmu.cs780.hw3;
+
+/**
+ * Hello world!
+ *
+ */
+public class Game 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ hw3/src/test/java/edu/cmu/cs780/hw3/GameTest.java
@@ -0,0 +1,20 @@
+package edu.cmu.cs780.hw3;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class GameTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
