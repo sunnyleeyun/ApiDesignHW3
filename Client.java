1fb8bf7ecc136276ba6db3e737cef26a916963ba
Zihao Yang <zihaoyang@Zihaos-MacBook-Pro-2.local>
2023-09-25 22:06:12 -0400

    add client code and javadoc

---
+++ Client.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        Game myGame = new Game();
+        System.out.println(myGame.prettyPrintBoard());
+        Scanner scanner = new Scanner(System.in);
+        int userInput = 0;
+
+        while (myGame.canGameContinue()){
+            while (true) {
+                System.out.print("Enter a number between 0 and 6: ");
+
+                // Check if the input is an integer
+                if (scanner.hasNextInt()) {
+                    userInput = scanner.nextInt();
+
+                    // Check if the number is within the desired range
+                    if (userInput >= 0 && userInput <= 6) {
+                        System.out.println("You entered a valid number: " + userInput);
+                        // Exit loop if input is valid
+                        break; 
+                    } else {
+                        System.out.println("Please enter a number between 0 and 6.");
+                    }
+                } else {
+                    System.out.println("Invalid input. Please enter a number.");
+                    // Clear invalid input
+                    scanner.next();
+                }
+            }
+            myGame.placeChecker(userInput);
+            System.out.println(myGame.prettyPrintBoard());
+        }
+
+        scanner.close();
+    }
+}
